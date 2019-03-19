dffa74be3bb4e87235307550e88fbd547c639f3e
jackson97010 <42036297+jackson97010@users.noreply.github.com>
2019-03-19 13:36:11 +0800

    Update Robot.java
    Periodic init 
    chassis slow

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,7 +34,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    
+    Chassis.PeriodicInit();
     Panel.autoInit();
     timer.reset();
     timer.start();
