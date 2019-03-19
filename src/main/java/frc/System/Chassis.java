804c1ade5f677a13a1f96f48d346bc948d00a62f
jackson97010 <42036297+jackson97010@users.noreply.github.com>
2019-03-19 13:34:12 +0800

    Update Chassis.java
    change the chassis

--- src/main/java/frc/System/Chassis.java
+++ src/main/java/frc/System/Chassis.java
@@ -12,34 +12,34 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Chassis {
 
 
-    //馬達的ID
+    //The ID of Port and checkt the MotorController ID
     private static final byte L_FrontMotor_ID = 0; 
-	private static final byte L_RearMotor_ID = 1;
-	private static final byte R_FrontMotor_ID = 2;
+    private static final byte L_RearMotor_ID = 1;
+    private static final byte R_FrontMotor_ID = 2;
     private static final byte R_RearMotor_ID = 3;
-
-    private static Init speed = 1;
     
-    //生成VictorSPX & WPI_VictorSPX 物件
+    //Create VictorSPX
     private static VictorSPX  L_RearMotor, R_RearMotor;
     private static WPI_VictorSPX L_FrontMotor, R_FrontMotor;
     
-    //生成分開控制物件
+    //Create DifferentailDrive
     private static DifferentialDrive m_Robot;
-    //是否一鍵直走
-    private static  boolean isSlow= true,isArcade = false;
+    
+    //whether Arcade
+    private static boolean isSlow= true;
     public static void Init(){
         L_FrontMotor = new WPI_VictorSPX(L_FrontMotor_ID);
         L_RearMotor = new VictorSPX(L_RearMotor_ID);
         R_FrontMotor = new WPI_VictorSPX(R_FrontMotor_ID);
         R_RearMotor = new VictorSPX(R_RearMotor_ID);
-        //DifferentialDrive 左邊是左邊 右邊是右邊
+        
+	//DifferentialDrive is left or right
         m_Robot = new DifferentialDrive(L_FrontMotor, R_FrontMotor);
-        //決定正轉還是反轉
-        //目前是相反
+        
+	//Inverted or not
         L_FrontMotor.setInverted(false);
-		L_RearMotor.setInverted(false);
-		R_FrontMotor.setInverted(false);
+	L_RearMotor.setInverted(false);
+	R_FrontMotor.setInverted(false);
         R_RearMotor.setInverted(false);
 
         L_RearMotor.setNeutralMode(NeutralMode.Brake);
@@ -48,88 +48,38 @@ public class Chassis {
         R_FrontMotor.setNeutralMode(NeutralMode.Brake);
 
         L_RearMotor.set(ControlMode.Follower, L_FrontMotor_ID);
-		R_RearMotor.set(ControlMode.Follower, R_FrontMotor_ID);  
+	R_RearMotor.set(ControlMode.Follower, R_FrontMotor_ID);  
     }
 
     public static void PeriodicInit(){
         isSlow = true;
-        // if (isArcade != false || isSlow != false){
-        //     isArcade = false;
-        //     isSlow = false;
-         
-        // }
         SmartDashboard.setDefaultBoolean("Chassis",true);
     }
-    //這段code仍然需要測試到底可不可行
+    
+    
     public static void tankDrive(){
         
         //should task whether it can work or not.要使用需先去Stcik改狀態
-        
-        // if (Stick.y){
-        //     isArcade =! isArcade;
-        // }
-        // else if (Stick.a){
-        //     isSlow =! isSlow;    
-        //     isArcade = false;
-        // }
-
-        // if(isArcade && Math.abs(Stick.ly+Stick.ry)/2 == 0){
-            
-        //     m_Robot.tankDrive(0.5,0.5);
-            
-
-        // }
-        // else if(isSlow){
-
-        //     m_Robot.tankDrive(Stick.ly*0.95, Stick.ry*0.95);     
-        
-        // }
-        // else {
-            
-        //     m_Robot.tankDrive(Stick.ly/2, Stick.ry/2);
-
-        // }
-        if(Stick.y){
-            m_Robot.tankDrive(0.5,0.5);
-            SmartDashboard.putNumber("Chassis", 3);
-        }  
-        else{
-            
-            /*if(Stick.a) {
-                isSlow =! isSlow;
+	    if(Stick.a){
+                isSlow != isSlow;
             }
+	    
             if(isSlow){
-                SmartDashboard.putNumber("Chassis", 2);
-                m_Robot.tankDrive(Stick.ly/2, Stick.ry/2);
+                SmartDashboard.putNumber("Chassis", 1);
+		m_Robot.tankDrive(Stick.ly/2, Stick.ry/2);
+
             }
             else {
-                SmartDashboard.putNumber("Chassis", 1);
-                m_Robot.tankDrive(Stick.ly*0.95, Stick.ry*0.95);        
-            }*/
-
-            if(Stick.a){
-                speed = speed + 1;
-            }
-            if(speed==3){
-                speed = speed - 2;
-            }
-            if(speed==1){
-                SmartDashboard.putNumber("Chassis", 1);
-                m_Robot.tankDrive(Stick.ly*0.95, Stick.ry*0.95);
-            }
-            else if(speed == 2){
                 SmartDashboard.putNumber("Chassis", 2);
-                m_Robot.tankDrive(Stick.ly/2, Stick.ry/2);
+		m_Robot.tankDrive(Stick.ly*0.95, Stick.ry*0.95);
+
             }
-        }
+
+        
     }        
 }
 
-    //     if (isArcade){
-    //         if(Stick.y){
-    //             m_Robot.tankDrive(0.5,0.5);
-              
-    //     }
+
 
  
     
